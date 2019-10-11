0fc5ea4c35cf084fd388f26b942a88b09ae06f4d
William Olewnicki <wolewnicki@yahoo.com>
2019-10-11 00:56:54 -0400

    Update play.py

--- play.py
+++ play.py
@@ -4,18 +4,30 @@ number = random.randint(1,20)
 name = input("What is your name?> ")
 
 print(f"Ok {name} I'm thinking of a number between 1 and 20.", '\n')
-guess = int(input("What is your first guess?> "))
 
-def game(guess):
-    if guess == number:
-        print("Correct!")
-    elif guess > number:
-        guess = int(input("Try a lower number> "))
-        game(guess)
-    else:
-        guess = int(input("Try a higher number> "))
-        game(guess)
-game(guess)
+while True:
+    try:
+        guess = int(input("What is your guess> "))
+    except ValueError:
+        print('\n', "Please enter a number", '\n')
+        continue
+    else: 
+        break
 
+#this is the main loop       
+while True:
+    try:
+        if guess == number:
+            print("Correct!")
+            break
+        elif guess > number: 
+            guess = int(input("Try a lower number> "))
+            continue
+        else:
+            guess = int(input("Try a higher number> "))
+            continue
+    except ValueError:
+            print('\n', "Invalid please try again", '\n')
+            continue
 #this is so if you run in cmd it stays open
 input()
