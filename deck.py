aaa429c4ed29c8e06788d953cfdc3787797b17b6
William Olewnicki <wolewnicki@yahoo.com>
2019-11-05 21:41:57 -0500

    Update deck.py

--- deck.py
+++ deck.py
@@ -4,6 +4,7 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+        self.value = rankValues[self.rank]
 
     def show(self):
         if self.rank == 1:
@@ -16,7 +17,7 @@ class Card:
             print(f"King of {self.suit}")
         else:
             print(f"{self.rank} of {self.suit}")
-
+    
 class Deck:
     def __init__(self):
         self.build()
@@ -40,14 +41,30 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
+        self.points = 0 
         
+    def hit(self):
+        topCard = deck.contents.pop()
+        self.hand.append(topCard)
+        self.pointCheck()
+        
+    def pHand(self):
+        for card in self.hand:
+            card.show()
+            
+    def pointCheck(self):
+        self.points = sum(self.hand.value)
+        return self.points
+    
 suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']        
 ranks = list(range(1,14))        
-rankValues = {1 : 11,
+rankValues = {1 : 11, 2 : 2, 3 : 3, 4 : 4, 5 : 5, 6 : 6, 7 : 7, 8 : 8, 9 : 9, 10 : 10,
               11 : 10,
               12 : 10,
               13 : 10}        
 
 deck = Deck()
 deck.shuffle()
-deck.show()
+player = Player('Will')
+player.hit()
+player.pHand()
